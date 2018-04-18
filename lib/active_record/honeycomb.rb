d7db9daad2f75f57e4973b0aadd18d444a46cacd
Sam Stokes <me@samstokes.co.uk>
2018-04-18 15:03:59 -0700

    Skeleton

---
+++ lib/active_record/honeycomb.rb
@@ -0,0 +1,4 @@
+module ActiveRecord
+  module Honeycomb
+  end
+end
---
+++ lib/active_record/honeycomb/version.rb
@@ -0,0 +1,6 @@
+module ActiveRecord
+  module Honeycomb
+    GEM_NAME = 'activerecord-honeycomb'
+    VERSION = '0.0.1'
+  end
+end
