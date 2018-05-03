61a03f43829a7a301db1e9b75880947ae5bcd004
Sam Stokes <me@samstokes.co.uk>
2018-05-03 15:11:07 -0700

    support kwargs protocol

--- lib/active_record/honeycomb.rb
+++ lib/active_record/honeycomb.rb
@@ -2,12 +2,13 @@ require 'active_record'
 
 module ActiveRecord
   module Honeycomb
-    def self.munge_config(config, client = nil)
+    def self.munge_config(config, client: nil, logger: nil)
       munged = config.merge(
         'adapter' => 'honeycomb',
         'real_adapter' => config.fetch('adapter'),
       )
       munged['honeycomb_client'] = client if client
+      munged['honeycomb_logger'] = logger if logger
       munged
     end
   end
--- lib/activerecord-honeycomb/auto_install.rb
+++ lib/activerecord-honeycomb/auto_install.rb
@@ -2,19 +2,19 @@ module ActiveRecord
   module Honeycomb
     module AutoInstall
       class << self
-        def available?
+        def available?(**_)
           gem 'activerecord'
         rescue Gem::LoadError
           false
         end
 
-        def auto_install!(honeycomb_client)
+        def auto_install!(honeycomb_client:, logger: nil)
           require 'active_record'
           require 'activerecord-honeycomb'
 
           ActiveRecord::Base.extend(Module.new do
             define_method :establish_connection do |config, *args|
-              munged_config = ActiveRecord::Honeycomb.munge_config(config, honeycomb_client)
+              munged_config = ActiveRecord::Honeycomb.munge_config(config, client: honeycomb_client, logger: logger)
               super(munged_config, *args)
             end
           end)
