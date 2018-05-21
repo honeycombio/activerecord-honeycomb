3d83f33511c06d08487641336c4ecb7010889549
Sam Stokes <me@samstokes.co.uk>
2018-05-21 14:46:08 -0500

    Test more cases, including non-working UPDATE and DELETE

--- spec/connection_adapters/honeycomb_adapter_spec.rb
+++ spec/connection_adapters/honeycomb_adapter_spec.rb
@@ -1,36 +1,93 @@
 require 'support/fakehoney'
 
+RSpec.shared_examples_for 'records a database query' do |name:, sql_match:, sql_not_match: nil|
+  it 'sends a db event' do
+    expect(last_event.data['type']).to eq('db')
+  end
+
+  it "sets 'name' to #{name.inspect} (although something more informative would be nicer!)" do
+    expect(last_event.data['name']).to eq(name)
+  end
+
+  it 'records the SQL query' do
+    expect(last_event.data).to include('db.sql' => sql_match)
+  end
+
+  it 'records the parameterised SQL query rather than the literal parameter values' do
+    expect(last_event.data['db.sql']).to_not match(sql_not_match)
+  end if sql_not_match
+
+  it 'records how long the statement took' do
+    expect(last_event.data['duration_ms']).to be_a Numeric
+  end
+
+  it 'includes meta fields in the event' do
+    expect(last_event.data).to include(
+      'meta.package' => 'activerecord',
+      'meta.package_version' => ActiveRecord::VERSION::STRING,
+    )
+  end
+end
+
 RSpec.describe 'ActiveRecord::ConnectionAdapters::HoneycombAdapter' do
   let(:last_event) { $fakehoney.events.last }
 
   context 'after a .create!' do
     before { Animal.create! name: 'Max', species: 'Lion' }
 
-    it 'sends a db event' do
-      expect(last_event.data['type']).to eq('db')
+    include_examples 'records a database query',
+      name: 'INSERT',
+      sql_match: /^INSERT INTO "animals"/,
+      sql_not_match: /Lion/
+  end
+
+  context 'after a .find' do
+    before do
+      Animal.create! name: 'Pooh', species: 'Bear'
+      @sanders = Animal.find_by(species: 'Bear')
     end
 
-    it 'sets "name" to "INSERT" (although something more informative would be nicer!)' do
-      expect(last_event.data['name']).to eq('INSERT')
+    include_examples 'records a database query',
+      name: 'SELECT',
+      sql_match: /^SELECT .* FROM "animals"/,
+      sql_not_match: /Bear/
+
+    it 'records how many records were returned' do
+      pending 'depends on underlying adapter API?'
+
+      expect(last_event.data['db.num_rows_returned']).to eq(1)
+    end
+  end
+
+  context 'after an update' do
+    before do
+      @robin = Animal.create! name: 'Robin Hood', species: 'Fox'
+      @robin.name = 'Sir Robert of Loxley'
+      @robin.save!
     end
 
-    it 'records the SQL query' do
-      expect(last_event.data).to include('db.sql' => /^INSERT INTO "animals"/)
+    it 'records a database query' do
+      pending 'need to hook in at a different level (ActiveRecord::ConnectionAdapters::DatabaseStatements#exec_update)'
+      fail "doesn't send events for UPDATE"
+
+      #name: 'UPDATE',
+      #sql_match: /^UPDATE "animals"/,
+      #sql_not_match: /Loxley/
+    end
+  end
+
+  context 'after a delete' do
+    before do
+      @robin = Animal.create! name: 'Robin Hood', species: 'Fox'
+      @robin.destroy!
     end
 
-    it 'records the parameterised SQL query rather than the literal parameter values' do
-      expect(last_event.data['db.sql']).to_not match(/Lion/)
-    end
+    it 'records a database query' do
+      pending 'need to hook in at a different level (ActiveRecord::ConnectionAdapters::DatabaseStatements#exec_delete)'
+      fail "doesn't send events for DELETE"
 
-    it 'records how long the statement took' do
-      expect(last_event.data['duration_ms']).to be_a Numeric
-    end
-
-    it 'includes meta fields in the event' do
-      expect(last_event.data).to include(
-        'meta.package' => 'activerecord',
-        'meta.package_version' => ActiveRecord::VERSION::STRING,
-      )
+      #name: 'DELETE',
+      #sql_match: /^DELETE FROM "animals"/
     end
   end
 
